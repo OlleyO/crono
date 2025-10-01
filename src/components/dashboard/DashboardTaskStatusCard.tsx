53b57c02f9699e869a7fe45d304566f3ff1774b7
Oleh Selivanov <my.mail.olech@gmail.com>
2025-10-01 22:21:01 +0200

    feat: create task status component
    - used shared ui config for task statuses

---
+++ src/components/dashboard/DashboardTaskStatusCard.tsx
@@ -0,0 +1,44 @@
+import cn from 'classnames'
+
+import type { TaskStatuses } from '@/entities/task'
+import { dashboardStatusesHashedByStatus } from '@/constants/dashboard'
+
+import WarningIcon from '@/assets/images/icons/warning.svg?react'
+
+interface DashboardTaskStatusCardProps {
+  count: number
+  errors?: number | null | undefined
+  status: TaskStatuses
+}
+
+export default function DashboardTaskStatusCard({
+  count,
+  status,
+  errors,
+}: DashboardTaskStatusCardProps) {
+  const uiConfig = dashboardStatusesHashedByStatus[status]
+
+  return (
+    <div className={cn('p-4 rounded-xl', uiConfig.bgColor)}>
+      <div className="flex justify-between">
+        <span className={cn('text-2xl font-medium', uiConfig.color)}>
+          {count}
+        </span>
+
+        {errors ? (
+          <div className="relative flex items-center gap-1.5 bg-white text-hover-red px-2 py-1 rounded-2xl -top-2">
+            <span className="text-xs font-medium">{errors} error</span>
+
+            <WarningIcon />
+          </div>
+        ) : undefined}
+      </div>
+
+      <div className="mt-2 flex items-center justify-between">
+        <span className="text-sm text-gray-hover-1 font-medium">
+          {uiConfig.label}
+        </span>
+      </div>
+    </div>
+  )
+}
