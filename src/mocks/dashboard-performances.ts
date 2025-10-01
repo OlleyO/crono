35e342adea94ce364603ad4e62c3fdfd9f2b127d
Oleh Selivanov <my.mail.olech@gmail.com>
2025-10-01 22:17:54 +0200

    feat: create mocks
    - performance section mocks
    - task statuses mocks

---
+++ src/mocks/dashboard-performances.ts
@@ -0,0 +1,32 @@
+export const dashboardPerformancesMock = [
+  {
+    total: 500,
+    count: 0,
+    type: 'contacts'
+  },
+  {
+    total: 500,
+    count: 0,
+    type: 'companies'
+  },
+  {
+    total: 2000,
+    count: 1000,
+    type: 'activities'
+  },
+  {
+    total: 30,
+    count: 20,
+    type: 'meetings'
+  },
+  {
+    total: 200,
+    count: 100,
+    type: 'deals'
+  },
+  {
+    total: 100,
+    count: 50,
+    type: 'pipeline'
+  }
+]
---
+++ src/mocks/dashboard-task-statuses.ts
@@ -0,0 +1,19 @@
+export const dashboardTaskStatusesMock = [
+  {
+     status: 'overdue',
+     count: 3
+  },
+  {
+    status: 'pending-auto',
+    count: 20,
+    errors: 1
+  },
+  {
+    status: 'pending-manual',
+    count: 10
+  },
+  {
+    status: 'completed',
+    count: 8
+  }
+]
