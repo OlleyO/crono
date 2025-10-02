fb0c8466b65adb8d48aab23c88b3c446d64fbe80
Oleh Selivanov <my.mail.olech@gmail.com>
2025-10-02 17:08:10 +0200

    feat: redirect user to /dashboard page from root page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: App,
+  beforeLoad: () => {
+    throw redirect({ to: '/dashboard' })
+  },
 })
 
 function App() {
